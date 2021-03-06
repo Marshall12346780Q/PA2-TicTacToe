02936025084ec3633f909ac159de87ae89e5b043
Ellis Levine <emlevine5@gmail.com>
2021-03-06 18:43:47 -0500

    Second Commit. Added a Reset button that will reset the game to the starting state.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 public class TicTacToe {
 
 	JButton[] buttons = new JButton[9];
+	JButton reset = new JButton();
 	JFrame frame = new JFrame();
 	JPanel topPane = new JPanel();
 	JPanel bottomPane = new JPanel();
@@ -54,6 +55,19 @@ public class TicTacToe {
 		}
 		
 		topPane.add(textfield);
+		topPane.add(reset);
+		reset.setText("RESET");
+		reset.addActionListener(new ActionListener() {
+			@Override 
+			public void actionPerformed(ActionEvent e) {
+				for(int i = 0; i < 9; i++) {
+					buttons[i].setEnabled(true);
+				}
+				for (int i = 0; i < 9; i++) {
+					buttons[i].setText("");
+				}
+			}
+		});
 		frame.add(topPane,BorderLayout.NORTH);
 		frame.add(bottomPane);
 		
@@ -73,7 +87,7 @@ public class TicTacToe {
 		if((buttons[0].getText().equals("O")) && (buttons[4].getText().equals("O")) && (buttons[8].getText().equals("O"))) {
 			hasWinner("O");
 		}
-		if((buttons[2].getText().equals("O")) && (buttons[4].getText()=="O") && (buttons[6].getText()=="O")) {
+		if((buttons[2].getText().equals("O")) && (buttons[4].getText().equals("O")) && (buttons[6].getText()=="O")) {
 			hasWinner("O");
 		}
 		if((buttons[0].getText().equals("O")) &&(buttons[1].getText().equals("O")) &&(buttons[2].getText().equals("O"))) {
@@ -124,6 +138,7 @@ public class TicTacToe {
 			textfield.setText("X wins");
 		if (winner.equals("O")) 
 			textfield.setText("O wins");
+		
 		
 	}
 	
