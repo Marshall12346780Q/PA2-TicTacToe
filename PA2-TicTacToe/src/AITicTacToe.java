a78a5d9171f04c65df249ccbd17e31de2aaaa55b
Marshall12346780Q <33814176+Marshall12346780Q@users.noreply.github.com>
2021-03-22 22:02:32 -0400

    PA3 With AI (in one folder)

--- PA2-TicTacToe/src/AITicTacToe.java
+++ PA2-TicTacToe/src/AITicTacToe.java
@@ -1,10 +1,10 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.util.*;
+import java.util.Random;
 import javax.swing.*;
 import javax.swing.Timer;
 
-public class TicTacToe {
+public class AITicTacToe {
 
 	public JButton[] buttons = new JButton[100];
 	protected JButton reset = new JButton();
@@ -21,14 +21,14 @@ public class TicTacToe {
     private int m;
     private int n;
     private int k;
-
-
+    private int winIndex = -1; //if not -1, a winning move has been found and is stored in this var for the AI to block
+    private int winIndexAI = -1; //if not -1, a winning move for the AI has been found and the AI will play it to win if empty
 
     public static void NewScreen() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					TicTacToe window = null;
+					AITicTacToe window = null;
 					window.frame.setVisible(false);
 				} catch (NullPointerException e) {
 					System.out.println("");
@@ -37,7 +37,7 @@ public class TicTacToe {
 		});
 	}
 
-	public TicTacToe(int m, int n, int k) {
+	public AITicTacToe(int m, int n, int k) {
 		this.m = m;
 		this.n = n;
 		this.k = k;
@@ -99,35 +99,30 @@ public class TicTacToe {
 					for(int i=0;i<m*n;i++) {
 						if(e.getSource()==buttons[i]) {
 							time = 15; //reset move timer
-							if(p1flag) {
-								if(buttons[i].getText()=="") {
-									buttons[i].setText("O");
-									p1flag=false;
-									textfield.setText("X's turn");
-									boolean check = checkForWinner(i,"O");
-									if (check)
+							if(buttons[i].getText()=="") 
+							{
+								buttons[i].setText("X");
+								p1flag=true;
+								textfield.setText("X's turn");
+								boolean check = checkForWinner(i,"X", true);
+								if (check)
+								{
+									textfield.setText("X WINS");
+									endGame();
+									t.stop();	
+								}
+								else
+								{	
+									if(checkForWinner(AIMove(), "O", false))
 									{
-										textfield.setText("O WINS");
+										textfield.setText("O (Computer) WINS");
 										endGame();
-										t.stop();	
+										t.stop();
 									}
+									
 								}
 							}
-							else {
-								if(buttons[i].getText()=="") {
-									buttons[i].setText("X");
-									p1flag=true;
-									textfield.setText("O's turn");
-									boolean check = checkForWinner(i,"X");
-									if (check)
-									{
-										textfield.setText("X WINS");
-										endGame();
-										t.stop();	
-									}
-
-								}
-							}
+						
 						}			
 					}
 				}
@@ -139,7 +134,7 @@ public class TicTacToe {
 			@Override 
 			public void actionPerformed(ActionEvent e) {
 				frame.dispose();
-				TicTacToe t = new TicTacToe(m, n, k);
+				AITicTacToe t = new AITicTacToe(m, n, k);
 			}
 		});		
 		returnToMenu.addActionListener(new ActionListener() {
@@ -157,46 +152,146 @@ public class TicTacToe {
 
 	}
 
-
-	protected boolean checkForWinner(int index, String s) //int index s is X or O
+	/*
+	 * If the winIndexAI is not -1, play it to win. 
+	 * If the winIndex is not -1, play it to block the player's win
+	 * Otherwise (first move) play a random empty square
+	 * Run checkForWinner() with AI flag
+	 * It's more efficient to store and check for played moves rather than check the whole array for empties
+	 * 
+	 * next version: run checkforWInner with a k of 1 more than ai's kcount to find next move
+	 */
+	private int AIMove()
 	{
+		int moveIndex = -1;
+		if(winIndexAI != -1)
+		{
+			moveIndex = winIndexAI;
+		}
+		else if(winIndex != -1)
+		{
+			moveIndex = winIndex;
+		}
+		else //random between 0 and m*n, if != "" try again
+		{
+			Random randMove = new Random();
+			moveIndex = randMove.nextInt((m*n));
+			while(!(buttons[moveIndex].getText().equals("")))
+			{
+				moveIndex = randMove.nextInt((m*n));
+			}
+		}
+		System.out.println("O Moves: " + moveIndex);
+		buttons[moveIndex].setText("O");
+		return moveIndex;
+		
+	}
+	
+	//needs a boolean parameter to indicate AI or player
+	protected boolean checkForWinner(int index, String s, boolean player) //int index s is X or O
+	{
+		//winIndex = -1;   //	DELET THOS
+		System.out.println(s+ "turn");
 		//Checking for solution involves checking for horizontal, vertical, or diagonal k-tuples
 		//If a row end, column end, or opponent/empty square is encountered, searching stops in that direction
 		//A count is kept for each of the three types. If it reaches k, a win is found
+		
+		//If it reaches k-1, this indicates the player is 1 move away from winning.
+		//The index of this winning move is saved so the AI knows to block it with it's turn
+		//This creates a naive AI that prevents k-1 tuples from becoming k tuples 
+		
 		int leftEnd = (index / m) * m;
 		int rightEnd = ((index / m + 1) * m )-1;
 		int kcount = 1; //contains tuple size, starts from 1 to include index
-
+		int openSquare = -1;
 		//HORIZONTAL CHECKING
 		for(int h = index-1; h >= leftEnd; h--) //checks for tuple going left
 		{
-			if(buttons[h].getText().equals(s)){kcount++;}
-			else{break;	}
+			if(buttons[h].getText().equals(s))
+			{
+				kcount++;
+			} 
+			else if (buttons[h].getText().equals(""))
+			{
+				openSquare = h; //indicate there is a potential winning move now in openSquare
+				break;
+			}
+			else{break;}
 		}
 		for(int h = index+1; h <= rightEnd; h++) //checks for tuple going right
 		{
-			if(buttons[h].getText().equals(s)){kcount++;}
+			if(buttons[h].getText().equals(s))
+			{
+				kcount++;
+			}
+			else if (buttons[h].getText().equals(""))
+			{
+				if(kcount == (k-1)) //now kcount may be properly checked
+				{
+					openSquare = h;
+				}
+				break;
+			}
 			else{break;}
 		}
+		if(kcount == (k-1)&&(openSquare != -1))
+		{
+			if(player == true)
+			{
+				winIndex = openSquare;
+			}
+			else if (player == false)
+			{
+				winIndexAI = openSquare;
+			}
+		}
 		if(kcount == k) {return true;}
-		kcount = 1;
+		
 		//VERTICAL CHECKING increments by m
+		kcount = 1;
+		openSquare = -1;
 		int topEnd = 0;
 		int bottomEnd = m*n;
-		for(int v = index-m; v > topEnd; v = v-m) //up
+		for(int v = index-m; v >= topEnd; v = v-m) //up
 		{
 			if(buttons[v].getText().equals(s)){kcount++;}
+			else if (buttons[v].getText().equals(""))
+			{
+				openSquare = v; 
+				break;
+			}
 			else{break;}
 		}
 		for(int v = index+m; v < bottomEnd; v = v+m) //down
 		{
 			if(buttons[v].getText().equals(s)){kcount++;}
+			else if (buttons[v].getText().equals(""))
+			{
+				if(kcount == (k-1)) //now kcount may be properly checked
+				{
+					openSquare = v;
+				}
+				break;
+			}
 			else{break;}
 		}
+		if(kcount == (k-1)&&(openSquare != -1))
+		{
+			if(player == true)
+			{
+				winIndex = openSquare;
+			}
+			else if (player == false)
+			{
+				winIndexAI = openSquare;
+			}
+		}
 		if(kcount == k) {return true;}
-		kcount = 0;
+		
 		//DIAGONAL CHECKING increments by m-1 and m+1, and stops on hitting a horizontal or vertical end
 		//down left and up right share count 
+		kcount = 0;
+		openSquare = -1;
 		for(int d = index; (d < (bottomEnd)); d = d+(m-1)) //down left
 		{
 			if(buttons[d].getText().equals(s))
@@ -207,6 +302,11 @@ public class TicTacToe {
 					break;
 				}
 
+			}
+			else if (buttons[d].getText().equals("")) 
+			{
+				openSquare = d; 
+				break; //must always break on a "" square
 			}
 			else{break;}
 		}
@@ -221,11 +321,31 @@ public class TicTacToe {
 					break;
 				}
 			}
+			else if (buttons[d].getText().equals(""))
+			{
+				if(kcount == (k-1))
+				{
+					openSquare = d; 
+				}
+				break;
+			}
 			else{break;}
 		}
+		if(kcount == (k-1)&&(openSquare != -1))
+		{
+			if(player == true)
+			{
+				winIndex = openSquare;
+			}
+			else if (player == false)
+			{
+				winIndexAI = openSquare;
+			}
+		}
 		if(kcount == k) {return true;}
-		kcount = 0;
 		//up left and down right share count
+		kcount = 0;
+		openSquare = -1;
 		for(int d = index; (d >= topEnd); d = d-(m+1)) //up left
 		{
 			if(buttons[d].getText().equals(s))
@@ -235,6 +355,11 @@ public class TicTacToe {
 				{
 					break;
 				}
+			}
+			else if (buttons[d].getText().equals("")) 
+			{
+				openSquare = d; 
+				break; //must always break on a "" square
 			}
 			else{break;}
 		}
@@ -249,14 +374,32 @@ public class TicTacToe {
 					break;
 				}
 			}
+			else if (buttons[d].getText().equals("")) 
+			{
+				if(kcount == (k-1))
+				{
+					openSquare = d; 
+				}
+				break; //must always break on a "" square
+			}
 			else{break;}
 		}
-		//System.out.println("kcount: " + kcount);
+		//if(kcount == (k-1)) {System.out.println("Imminent Win: " + openSquare);}
+		if(kcount == (k-1)&&(openSquare != -1))
+		{
+			if(player == true)
+			{
+				winIndex = openSquare;
+			}
+			else if (player == false)
+			{
+				winIndexAI = openSquare;
+			}
+		}
 		if(kcount == k) {return true;}
-
+		//System.out.println("Imminent Win: " + winIndex);
 		return false;
 	}
-
 
 	private boolean isMultiple(int val, int increment, int offset) //returns true if val + offset is a multiple of increment
 	{
